8d12a27e5ebfdeb313de57ab50e3b038bfc434fe
DevPabloOliveira <pablosiegfried@gmail.com>
2025-11-17 21:40:49 -0400

    ✨ feat(ci): adiciona testes unitários com Jest e workflow de GitHub Actions

---
+++ tests/publicController.test.js
@@ -0,0 +1,24 @@
+// 1. Importa a função específica que queremos testar
+const { getDbTodayDate } = require('../src/controllers/publicController');
+
+// "describe" agrupa um conjunto de testes relacionados ao "publicController"
+describe('Testes para Funções Auxiliares (Helpers) do publicController', () => {
+
+    // "test" define um caso de teste individual
+    test('getDbTodayDate deve retornar a data no formato YYYY-MM-DD', () => {
+        
+        // 2. Chama a função que está sendo testada
+        const dateString = getDbTodayDate();
+
+        // 3. Define o padrão esperado (Expressão Regular para YYYY-MM-DD)
+        const regex = /^\d{4}-\d{2}-\d{2}$/;
+
+        // 4. "expect" é a asserção (o que esperamos que aconteça)
+        // Esperamos que a string de data (dateString) corresponda (toMatch) à regex.
+        expect(dateString).toMatch(regex);
+    });
+
+    // Pode-se adicionar mais testes aqui no futuro...
+    // exemplo : test('outroTeste deve fazer outra coisa', () => { ... });
+
+});
